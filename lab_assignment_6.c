53cdd62207db3ce790195071d2077d510f613817
Shaquille <shaquillemiller533@gmail.com>
2024-02-22 18:35:37 -0500

    Shaquille Miller Lab 6

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,27 @@
+/*
+	COP3502C-24 Spring 0023 Lab 5
+	Copyright 2024 Shaquille_Miller
+*/
+
 #include <stdio.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	//Ensure high is higher than low, or item is not found
+	if(high<low) return -1;
+
+	//Since array is sorted, i will find the middle first and decide if i should search left or right
+	int middleIndex = (high+low)/2;
+	int middleVal = numbers[middleIndex];
+
+	//Check if middle val is the value we are looking for 
+	if(middleVal == value){
+		return middleIndex;
+	}else if(middleVal>value){ //If middle value is greater than what we are looking for then search left
+		return search(numbers,low,middleIndex-1,value);
+	}else{ //Middle value is less then value we are looking for then search right
+		return search(numbers,middleIndex+1,high,value);
+	}
 }
 
 void printArray(int numbers[], int sz)
